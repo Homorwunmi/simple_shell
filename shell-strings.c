b5291a0c9ac4d8eb46b5d1966ac572ddb2d5bd61
Hormorwunmi <omotayomowunmi@gmail.com>
2023-09-20 16:38:14 -0700

    strings

--- shell-strings.c
+++ shell-strings.c
@@ -1,47 +1,47 @@
 #include "shell.h"
 
 /**
- * _strncmp - Compares two strings up to n bytes.
+ * compare_strings - Compares two strings up to n bytes.
  * @s1: First string.
  * @s2: Second string.
  * @n: Number of bytes to be compared.
  * Return: 0 if strings match up to n bytes, 1 otherwise.
  */
-int _strncmp(char *s1, char *s2, size_t n)
+int compare_strings(char *s1, char *s2, size_t n)
 {
-	size_t a;
+	size_t idx;
 
 	if (!s1 || !s2)
 	return (1);
 
-	for (a = 0; a < n; a++)
+	for (idx = 0; idx < n; idx++)
 	{
-		if (s1[a] != s2[a])
+		if (s1[idx] != s2[idx])
 		return (1);
 	}
 	return (0);
 }
 
 /**
- * _strdup - Creates a duplicate of the given string.
- * @str: String to be duplicated.
+ * string_dup - Creates a duplicate of the provided string.
+ * @src: Source string to be duplicated.
  * Return: Pointer to the duplicated string or NULL if memory allocation fails.
  */
-char *_strdup(char *str)
+char *string_dup(char *src)
 {
-	int a, len;
+	int idx, length;
 	char *duplicate;
 
-	len = strlen(str);
-	duplicate = malloc(sizeof(char) * (len + 1));
+	length = strlen(src);
+	dup_str = malloc(sizeof(char) * (length + 1));
 	/* +1 for the null terminator*/
 
-	if (!duplicate)
+	if (!dup_str)
 	return (NULL);
 
-	for (a = 0; str[a]; a++)
-	duplicate[a] = str[a];
-	duplicate[a] = '\0';
+	for idx = 0; src[a]; idx++)
+	duplicate[idx] = str[idx];
+	duplicate[idx] = '\0';
 
-	return (duplicate);
+	return (dup_str);
 }
