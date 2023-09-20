38f8ae18b7f54894accc70b70cba1c49421d1904
Hormorwunmi <omotayomowunmi@gmail.com>
2023-09-20 03:09:50 -0700

    path

---
+++ shell-path.c
@@ -0,0 +1,19 @@
+#include "shell.h"
+
+/**
+ * get_env_path - Retrieves and duplicates the environment PATH.
+ * Return: A string of the PATH variable or NULL if not found.
+ */
+char *get_env_path(void)
+{
+	int a;
+	char *duplicate = NULL;
+
+	for (a = 0; environ[a] != NULL; a++)
+	{
+		if (strncmp(environ[a], "PATH", 4) == 0)
+		duplicate = strdup(environ[a]);
+	}
+	/*Free this return value if it's not NULL */
+	return (duplicate);
+}
