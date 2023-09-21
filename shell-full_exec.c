f06702d266606a24b84f13c071dd801acb1625b7
Hormorwunmi <omotayomowunmi@gmail.com>
2023-09-21 02:22:27 -0700

    full exec

--- shell-full_exec.c
+++ shell-full_exec.c
@@ -1,42 +1,42 @@
 #include "shell.h"
 
 /**
- * execute_full_command - Executes a specified full command.
- * @args: The command arguments
- *	(first element typically contains the command itself).
- * @argv: Array of original command line arguments (used for error reporting).
- * @command_path: Full path of the command to execute.
- * Return: Status of the executed command or error code.
+ * run_full_cmd - Executes a specified full command.
+ * @cmd_args: Arguments for the command
+ *	(first element usually represents the command itself).
+ * @cmd_argv: Array of initial command line arguments for error notifications.
+ * @full_cmd_path: Full path of the command to be initiated.
+ *
+ * Return: Status of the initiated command or error code.
  */
-int execute_full_command(char **args, char **argv, char *command_path)
+int run_full_cmd(char **cmd_args, char **cmd_argv, char *full_cmd_path)
 {
-	pid_t num;
-	int status;
+	pid_t childProcess;
+	int cmd_status;
 
-	num = fork();
-	if (num < 0)
+	childProcess = fork();
+	if (childProcess < 0)
 	{
-		perror(command_path);
+		perror(full_cmd_path);
 		exit(-1);
 	}
-	else if (num == 0)
+	else if (childProcess == 0)
 	{
-		execve(command_path, args, environ);
-		perror(argv[0]);
+		execve(full_cmd_path, cmd_args, environ);
+		perror(cmd_argv[0]);
 		exit(2);
 	}
 	else
 	{
-		wait(&status);
-		/* Correct conversion of the child process's exit status */
+		wait(&cmd_status);
 		if (WIFEXITED(status))
-			status = WEXITSTATUS(status);
+			cmd_status = WEXITSTATUS(cmd_status);
 
-		errno = status;
+		errno = cmd_status;
 
-		free(command_path);
-		free(args);
+		free(full_cmd_path);
+		free(cmd_args);
 	}
 
-	return (status);
+	return (cmd_status);
 }
