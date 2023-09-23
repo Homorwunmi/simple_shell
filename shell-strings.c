dc61b827731b674b17809fa8d659a1c8ee73f153
Hormorwunmi <omotayomowunmi@gmail.com>
2023-09-22 17:42:27 -0700

    strings

--- shell-strings.c
+++ shell-strings.c
@@ -5,6 +5,7 @@
  * @s1: First string.
  * @s2: Second string.
  * @n: Number of bytes to be compared.
+ *
  * Return: 0 if strings match up to n bytes, 1 otherwise.
  */
 int compare_strings(char *s1, char *s2, size_t n)
@@ -23,8 +24,43 @@ int compare_strings(char *s1, char *s2, size_t n)
 }
 
 /**
+ * compareStrings - compares two strings
+ * @s1: first string.
+ * @s2: second string.
+ *
+ * Return: 0 if the strings are equal, a +ve value if s1 is greater,
+ * and a -ve value if s2 is greater.
+ */
+int compareStrings(const char *s1, const char *s2)
+{
+    size_t idx;
+
+    if (s1 == NULL && s2 == NULL)
+        return (0);
+    if (s1 == NULL)
+        return (-1);
+    if (s2 == NULL)
+        return (1);
+
+    for (idx = 0; s1[idx] != '\0' || s2[idx] != '\0'; idx++)
+    {
+        if (s1[idx] == '\0' && s2[idx] != '\0')
+            return (-1);
+        if (s1[idx] != '\0' && s2[idx] == '\0')
+            return (1);
+        if (s1[idx] > s2[idx])
+            return (1);
+        if (s1[idx] < s2[idx])
+            return (-1);
+    }
+    return (0);
+}
+
+
+/**
  * string_dup - Creates a duplicate of the provided string.
  * @src: Source string to be duplicated.
+ *
  * Return: Pointer to the duplicated string or NULL if memory allocation fails.
  */
 char *string_dup(char *src)
