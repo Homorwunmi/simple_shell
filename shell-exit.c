77381b6934f7688f9e419be4a9250d9644046c74
Hormorwunmi <omotayomowunmi@gmail.com>
2023-09-22 17:22:14 -0700

    exit

---
+++ shell-exit.c
@@ -0,0 +1,33 @@
+#include "shell.h"
+
+/**
+ * custom_exit - Exits the shell with a customed exit status.
+ * @args: Array of tokens containing the exit status.
+ *
+ * Return: 0 (Success)
+ */
+int custom_exit(char **args)
+{
+	int status = 0;
+
+	/* If an exit status is provided, validate and set it.*/
+	if (args[1] != NULL)
+	{
+		if (_strcmp(args[1], "0") == 0)
+		{
+			status = 0;
+		}
+		else
+		{
+			status = atoi(args[1]);
+			if (status <= 0)
+			{
+				write(STDERR_FILENO, "Invalid exit status\n", 20);
+				return (1);
+			}
+		}
+	}
+
+	exit(exit_status);
+}
+
